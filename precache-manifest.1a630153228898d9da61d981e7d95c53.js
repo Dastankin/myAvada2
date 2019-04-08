d60a1839ff92b0c4b0c1da9608543d354062e6c5
Dastankin <hoaivong123@gmail.com>
2019-04-08 22:36:13 +0700

    updoad again

---
+++ precache-manifest.1a630153228898d9da61d981e7d95c53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e93433897e9c03c5964c",
+    "url": "/myAvada/static/js/runtime~main.e9343389.js"
+  },
+  {
+    "revision": "2d305e1d83db326a16a1",
+    "url": "/myAvada/static/js/main.2d305e1d.chunk.js"
+  },
+  {
+    "revision": "9c6c19e4e2bd5f3c372a",
+    "url": "/myAvada/static/js/2.9c6c19e4.chunk.js"
+  },
+  {
+    "revision": "2d305e1d83db326a16a1",
+    "url": "/myAvada/static/css/main.27e8c94c.chunk.css"
+  },
+  {
+    "revision": "cf205f2a8c3a87c5c005104dca2b4ada",
+    "url": "/myAvada/index.html"
+  }
+];
